4d780d3975143994bffabe39a9f544542c6ba82c
jonathan1209-ppap <jona5427@icloud.com>
2024-10-01 10:20:33 +0900

    Create 11724.cc

---
+++ Silver/11724.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int> >Vec;
+vector<bool>is_visited;
+
+void DFS(int v){
+    is_visited[v] = 1;
+    for(auto i :Vec[v]){
+        if(is_visited[i]) continue;
+        DFS(i);
+    }
+}
+int main()
+{
+    int n,m;
+    int cnt=0;
+
+    cin >> n;
+    cin >> m;
+    Vec.resize(n+1);
+    is_visited.resize(n+1);
+    for (int i = 0; i < m; i++) {
+        int a,b;
+
+        cin >> a >> b;
+
+        Vec[a].push_back(b);
+        Vec[b].push_back(a);
+    }
+
+    for (int i=1;i<=n;i++){
+        if (!is_visited[i]){
+            DFS(i);cnt++;
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}
