f035610b554f7db2d960381c8009a7e724429c23
jonathan1209-ppap <jona5427@icloud.com>
2024-10-01 10:17:56 +0900

    Create 10815.cc

---
+++ Silver/10815.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <vector> 
+using namespace std;
+
+vector<int> a;
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
+  
+  int n,m;
+  
+  cin >> n;
+  for(int i=0;i<n;i++){
+    int t;
+    cin >> t;
+    a.push_back(t);
+  }
+  sort(a.begin(),a.end());
+  cin >> m;
+  for(int i=0;i<m;i++){
+    int x;
+    cin >> x;
+    cout << binary_search(a.begin(),a.end(),x) << " ";
+  }
+}
